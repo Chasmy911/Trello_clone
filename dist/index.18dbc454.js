66d46f9e412e2497cc1050d28b6a4230a621d6cb
chasmy911 <chasmy@inbox.ru>
2023-06-06 15:12:09 +0300

    add block styles

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -574,6 +574,45 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+// create main block
+const mainContainer = document.getElementById("main_block");
+const todoBlock = document.createElement("div");
+const progressBlock = document.createElement("div");
+const doneBlock = document.createElement("div");
+const todoBlockHeader = document.createElement("div");
+const progressBlockHeader = document.createElement("div");
+const doneBlockHeader = document.createElement("div");
+const todoBlockHeaderTitle = document.createElement("h2");
+const progressBlockHeaderTitle = document.createElement("h2");
+const doneBlockHeaderTitle = document.createElement("h2");
+const todoBlockHeaderAmount = document.createElement("div");
+const progressBlockHeaderAmount = document.createElement("div");
+const doneBlockHeaderAmount = document.createElement("div");
+const todoBlockContainer = document.createElement("div");
+const progressBlockContainer = document.createElement("div");
+const doneBlockContainer = document.createElement("div");
+const todoBlockBtn = document.createElement("button");
+const doneBlockBtn = document.createElement("button");
+mainContainer.append(todoBlock, progressBlock, doneBlock);
+todoBlock.append(todoBlockHeader, todoBlockContainer, todoBlockBtn);
+progressBlock.append(progressBlockHeader, progressBlockContainer);
+doneBlock.append(doneBlockHeader, doneBlockContainer, doneBlockBtn);
+todoBlockHeader.append(todoBlockHeaderTitle, todoBlockHeaderAmount);
+progressBlockHeader.append(progressBlockHeaderTitle, progressBlockHeaderAmount);
+doneBlockHeader.append(doneBlockHeaderTitle, doneBlockHeaderAmount);
+todoBlock.classList.add("todoBlock");
+progressBlock.classList.add("progressBlock");
+doneBlock.classList.add("doneBlock");
+todoBlockHeader.classList.add("todoBlockHeader");
+progressBlockHeader.classList.add("progressBlockHeader");
+doneBlockHeader.classList.add("doneBlockHeader");
+todoBlockBtn.classList.add("todoBlockBtn");
+doneBlockBtn.classList.add("doneBlockBtn");
+todoBlockHeaderTitle.innerText = "TODO:";
+progressBlockHeaderTitle.innerText = "IN PROGRESS:";
+doneBlockHeaderTitle.innerText = "DONE:";
+todoBlockBtn.innerText = "ADD TODO";
+doneBlockBtn.innerText = "DELETE ALL";
 
 },{}]},["gEwwu","1SICI"], "1SICI", "parcelRequire1fb0")
 
